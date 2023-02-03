13e444040da5848759c5dcbbf2f06a67efc2b853
leeteayu <ehdrkq2005@naver.com>
2023-02-03 14:54:11 +0900

    23.02.03

--- js/main.js
+++ js/main.js
@@ -2,44 +2,53 @@ $(function(){
     let wH = window.innerHeight;
     let scTop = 0;
 
-    setTimeout(function(){
-        $(".hi").addClass("in");
-    },400)
-    setTimeout(function(){
+    $(".leetaeyu").click(function(){
         $(".box").addClass("out");
-    },1700)
-    setTimeout(function(){
         $(".box").addClass("none");
-    },2800)
-    setTimeout(function(){
         $(".home_wrap").addClass("on");
-    },2700)
+    })
+
+
+    setTimeout(function(){
+        $(".wrap").addClass("in");
+    },400)
+    // setTimeout(function(){
+    //     $(".box").addClass("out");
+    // },1700)
+    // setTimeout(function(){
+    //     $(".box").addClass("none");
+    // },2800)
+    // setTimeout(function(){
+    //     $(".home_wrap").addClass("on");
+    // },2700)
 
     $("#pf_main_container").fullpage({
         navigation:false,
     });
 
-    var skillSwiper = new Swiper(".skill_swiper", {
+    var skillsSwiper = new Swiper(".skills_swiper", {
         slidesPerView: 6,
         speed:800,
         // centeredSlides: true,
         infinite:true,
         loop:true,
+        loopAdditionalSlides: 1,
+        allowTouchMove: true,
         autoplay: {
             delay: 2000,
-            disableOnInteraction: false,
+            disableOnInteraction: true,
         },
     });
     var bannerSwiper = new Swiper(".banner_swiper", {
         slidesPerView: 1,
-        initialSlide: 3,
         speed:800,
         centeredSlides: true,
         infinite:true,
         loop:true,
+        loopAdditionalSlides: 1,
         allowTouchMove: false,
         autoplay: {
-            delay: 5000,
+            delay: 7000,
             disableOnInteraction: true,
         },
         navigation: {
@@ -47,7 +56,19 @@ $(function(){
             prevEl: ".prev",
         },
     });
-    
+
+    $('.home_container').on('mouseover', function(){
+        bannerSwiper.autoplay.stop();
+    });
+    $('.home_container').on('mouseout', function(){
+        bannerSwiper.autoplay.start();
+    });
+    $('.box').on('mouseover', function(){
+        bannerSwiper.autoplay.stop();
+    });
+    $('.box').on('mouseout', function(){
+        bannerSwiper.autoplay.start();
+    });
 
     $(".link_1 > a").mouseenter(function(){
         $(".right_1").addClass("enter");
@@ -91,4 +112,11 @@ $(function(){
     $(".link_7 > a").mouseleave(function(){
         $(".right_7").removeClass("enter");
     });
+
+    // const link = $('.banner_swiper .slidebox .link')
+    // for (let i = 0; i < link.length; i++) {
+    //     $(".link_ " + i +" > a").mouseenter(function(){
+    //         $(".right_1").addClass("enter");
+    //     });
+    // }
 })
